fee4add9f41b1767ec2b5b4e60a52299288367af
Travis Carson <tlc@traviss-mbp.home>
2018-12-06 23:34:28 -0600

    Style raw json component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,21 @@
 import React, { Component } from "react";
 import "./App.css";
 
+import BlockInfo from "../BlockInfo/BlockInfo";
+import RawBlock from "../RawBlock/RawBlock";
+import dummyBlocks from "../../dummyBlocks";
+import dummyRawJson from "../../dummyRawJson";
+
 class App extends Component {
   render() {
-    return <div className="App">hi</div>;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <BlockInfo info={dummyBlocks} />
+          <RawBlock rawBlock={dummyRawJson} />
+        </header>
+      </div>
+    );
   }
 }
 
--- src/components/RawBlock/RawBlock.js
+++ src/components/RawBlock/RawBlock.js
@@ -1,8 +1,15 @@
 import React from "react";
+import ReactJson from "react-json-view";
 import "./RawBlock.css";
 
-const RawBlock = props => {
-  return <p>raw json will go here</p>;
-};
+const RawBlock = props => (
+  <div className="raw-block-data">
+    <ReactJson
+      className="raw-block-json"
+      style={{ "word-wrap": "break-word" }}
+      src={props.rawBlock}
+    />
+  </div>
+);
 
 export default RawBlock;
