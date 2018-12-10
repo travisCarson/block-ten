90d7bd1e64dababf591d0046a1caf4f5290d4679
Travis Carson <tlc@traviss-mbp.home>
2018-12-09 20:17:06 -0600

    Add port to config

--- server/config.js
+++ server/config.js
@@ -1,9 +1,10 @@
 module.exports = {
   remoteUrl: "http://api.eosnewyork.io",
-  remotePort: "9876",
+  port: 1111,
+  remotePort: 9876,
   rpcConfig: {
     rpcUrl: "http://127.0.0.1",
-    rpcPort: "8888",
+    rpcPort: 8888,
     maxRetryAttempts: 5
   }
 };
